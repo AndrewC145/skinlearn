2b18dcf5482977e406946d9354c3bf91fc0330d6
Andrew Cao <andrewcao618@gmail.com>
2025-11-28 17:52:47 -0500

    Added header utils

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,15 @@
 import { Link } from "react-router";
 import { User } from "lucide-react";
+import { useAuth } from "../context/AuthContext";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "../components/ui/dropdown-menu";
+import { Button } from "../components/ui/button";
 
 function Header() {
   const headerLinks = [
@@ -15,11 +25,6 @@ function Header() {
       link: "/routine-builder",
       name: "Routine Builder",
     },
-    {
-      link: "/login",
-      icon: <User className="text-black" />,
-      label: "Account",
-    },
   ];
 
   return (
@@ -27,20 +32,72 @@ function Header() {
       <nav className="px-6 py-4 text-black">
         <ul className="font-figtree flex items-center justify-between">
           <li>
-            <h1 className="text-4xl font-semibold">SKINLEARN</h1>
+            <Link to="/">
+              <h1 className="text-4xl font-semibold">SKINLEARN</h1>
+            </Link>
           </li>
-          <ul className="flex gap-8">
+          <ul className="flex items-center justify-center gap-8">
             {headerLinks.map((link, index) => (
               <li className="font-light hover:underline" key={index}>
-                <Link aria-label={link.label} to={link.link}>
-                  {link.name ? link.name : link.icon}
-                </Link>
+                <Link to={link.link}>{link.name}</Link>
               </li>
             ))}
+            <HeaderDropDown />
           </ul>
         </ul>
       </nav>
     </header>
+  );
+}
+
+function HeaderDropDown() {
+  const { user, handleLogout } = useAuth();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Button
+          className="cursor-pointer border-none bg-transparent shadow-none hover:bg-transparent"
+          variant="outline"
+        >
+          <User className="size-5" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-30" align="start">
+        {user ? (
+          <>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuGroup>
+              <MenuItemHelper text="Log out" onClick={handleLogout} />
+            </DropdownMenuGroup>
+          </>
+        ) : (
+          <>
+            <DropdownMenuGroup>
+              <Link to="/register">
+                <MenuItemHelper text="Register" />
+              </Link>
+              <Link to="/login">
+                <MenuItemHelper text="Sign In" />
+              </Link>
+            </DropdownMenuGroup>
+          </>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+function MenuItemHelper({
+  text,
+  onClick,
+}: {
+  text: string;
+  onClick?: () => void;
+}) {
+  return (
+    <DropdownMenuItem onClick={onClick} className="cursor-pointer">
+      {text}
+    </DropdownMenuItem>
   );
 }
 
